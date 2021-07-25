3a93934f8e4211c391d6558545a90b0136a0afc2
medhat <medhatusama@gmail.com>
2021-07-25 15:16:33 +0300

    configured allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7duzpkaqyucs$54abx(c1hcwr*@0u#@^r3n)$3d3^5^l*u-8ps'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
